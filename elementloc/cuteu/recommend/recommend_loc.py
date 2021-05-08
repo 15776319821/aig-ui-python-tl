11e9bc785f048950ff0f336115bd566b7046867b
tianlei <lei.tian@asiainnovations.com>
2021-05-08 19:06:40 +0800

    推荐-非会员点击发现筛选按钮

---
+++ elementloc/cuteu/recommend/recommend_loc.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.common.by import By
+
+class ElementLoc:
+
+    #会员拦截弹窗
+    vip_intercept_loc = (By.ID, 'com.cuteu.videochat:id/tvTitle')
+    #会员拦截弹窗
+    vip_intercept_close_loc = (By.ID, 'com.cuteu.videochat:id/tvClose')
+
+
+
+    #一级页面推荐按钮
+    recommend_loc = (By.ID, 'com.cuteu.videochat:id/btnTabRecommend')
+    #推荐页-发现按钮
+    discover_loc = (By.XPATH, '//android.widget.LinearLayout[@content-desc="发现"]/android.view.ViewGroup/android.widget.TextView')
+    #推荐页-发现-筛选按钮
+    discover_screen_loc = (By.ID, 'com.cuteu.videochat:id/countryTv')
+    #推荐页-发现-筛选list
+    discover_screen_list_loc = (By.ID, 'com.cuteu.videochat:id/nameTv')
+    #推荐页-发现-筛选关闭按钮
+    discover_screen_close_loc = (By.ID, 'com.cuteu.videochat:id/closeBarView')
+    #推荐页-发现-卡片
+    discover_screen_card_loc = (By.ID, 'com.cuteu.videochat:id/bgView')
+    # 推荐页-发现-通话按钮
+    discover_screen_call_loc = (By.ID, 'com.cuteu.videochat:id/callView')
---
+++ pageproject/cuteu/recommend/discover_page.py
@@ -0,0 +1,52 @@
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+
+sys.path.append(os.getcwd())
+import time
+from elementloc.cuteu.recommend.recommend_loc import ElementLoc
+from base.base_action import BaseAction
+
+class DiscoverPage(BaseAction):
+    #
+    # def __init__(self,driver):
+    #     BaseAction.__init__(self,driver)
+    def recommend(self):
+        #q=self.driver.find_elemen
+        # t(By.ID,'com.cuteu.videochat:id/btnTabMessage')
+        #self.find_element(ElementLoc.match_loc1).click()
+        self.click_element(ElementLoc.recommend_loc,"点击推荐")
+        # print("页面级方法1")
+        time.sleep(3)
+    def discover(self):
+        self.click_element(ElementLoc.discover_loc, '点击推荐-发现按钮')
+        time.sleep(3)
+
+    def discover_screen(self):
+        self.click_element(ElementLoc.discover_screen_loc, '点击推荐-发现-筛选国家按钮')
+        time.sleep(3)
+
+    def discover_screen_list(self, type):
+        self.click_elements(ElementLoc.discover_screen_list_loc, type, "点击第{}个国家".format(type))
+        time.sleep(3)
+
+    def discover_screen_close(self):
+        self.click_element(ElementLoc.discover_screen_close_loc, '点击推荐-发现-筛选国家按钮关闭按钮')
+        time.sleep(3)
+
+    def discover_screen_card(self):
+        self.click_element(ElementLoc.discover_screen_card_loc, '点击推荐-发现-卡片')
+        time.sleep(3)
+
+    def discover_screen_call(self):
+        self.click_element(ElementLoc.discover_screen_call_loc, '点击推荐-发现-通话按钮')
+        time.sleep(3)
+
+    def vip_intercept(self):
+        self.find_element(ElementLoc.vip_intercept_loc, '出现会员拦截弹窗')
+        time.sleep(3)
+
+    def vip_intercept_close(self):
+        self.click_element(ElementLoc.vip_intercept_close_loc, '点击关闭会员拦截弹窗')
+        time.sleep(3)
