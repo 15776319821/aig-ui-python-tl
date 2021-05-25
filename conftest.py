4e0ea4da9480f550d5b8e6af08861af091fe04e7
hongbin.han <hongbin.han@asiainnovations.com>
2021-05-25 15:36:42 +0800

    修改

--- conftest.py
+++ conftest.py
@@ -28,3 +28,88 @@ def returnBtn(self):
             break
 def startScreenShot(dirver):
     pass
+def returnBtn(self):
+    logger.info("返回")
+    #self.find_element(ImLoc.Im_news_1v1message_returnBtn,"返回按钮")
+
+    returnBtn=ImLoc.Im_news_1v1message_returnBtn
+    source=self.driver.page_source
+    time.sleep(2)
+    while True:
+        if returnBtn[1] in source:
+
+            self.click_element(ImLoc.Im_news_1v1message_returnBtn,'返回按钮')
+            logger.info("循环点击返回按钮，退出到一级页面")
+        else:
+            logger.info("页面返回到一级页面")
+            break
+def startScreenShot(dirver):
+    pass
+def returnBtn(self):
+    logger.info("返回")
+    #self.find_element(ImLoc.Im_news_1v1message_returnBtn,"返回按钮")
+
+    returnBtn=ImLoc.Im_news_1v1message_returnBtn
+    source=self.driver.page_source
+    time.sleep(2)
+    while True:
+        if returnBtn[1] in source:
+
+            self.click_element(ImLoc.Im_news_1v1message_returnBtn,'返回按钮')
+            logger.info("循环点击返回按钮，退出到一级页面")
+        else:
+            logger.info("页面返回到一级页面")
+            break
+def startScreenShot(dirver):
+    pass
+def returnBtn(self):
+    logger.info("返回")
+    #self.find_element(ImLoc.Im_news_1v1message_returnBtn,"返回按钮")
+
+    returnBtn=ImLoc.Im_news_1v1message_returnBtn
+    source=self.driver.page_source
+    time.sleep(2)
+    while True:
+        if returnBtn[1] in source:
+
+            self.click_element(ImLoc.Im_news_1v1message_returnBtn,'返回按钮')
+            logger.info("循环点击返回按钮，退出到一级页面")
+        else:
+            logger.info("页面返回到一级页面")
+            break
+def startScreenShot(dirver):
+    pass
+def returnBtn(self):
+    logger.info("返回")
+    #self.find_element(ImLoc.Im_news_1v1message_returnBtn,"返回按钮")
+
+    returnBtn=ImLoc.Im_news_1v1message_returnBtn
+    source=self.driver.page_source
+    time.sleep(2)
+    while True:
+        if returnBtn[1] in source:
+
+            self.click_element(ImLoc.Im_news_1v1message_returnBtn,'返回按钮')
+            logger.info("循环点击返回按钮，退出到一级页面")
+        else:
+            logger.info("页面返回到一级页面")
+            break
+def startScreenShot(dirver):
+    pass
+def returnBtn(self):
+    logger.info("返回")
+    #self.find_element(ImLoc.Im_news_1v1message_returnBtn,"返回按钮")
+
+    returnBtn=ImLoc.Im_news_1v1message_returnBtn
+    source=self.driver.page_source
+    time.sleep(2)
+    while True:
+        if returnBtn[1] in source:
+
+            self.click_element(ImLoc.Im_news_1v1message_returnBtn,'返回按钮')
+            logger.info("循环点击返回按钮，退出到一级页面")
+        else:
+            logger.info("页面返回到一级页面")
+            break
+def startScreenShot(dirver):
+    pass
